1ab592c2d843cda2208c5a806ed3bc9f3afd7801
papu163 <182830006+papu163@users.noreply.github.com>
2025-07-15 16:11:33 -0600

    Update helper.sh

--- helper.sh
+++ helper.sh
@@ -2,9 +2,12 @@
 
 #You can use others but first test and check with wtype -k your_bind in your terminal
 #And also you need to check that the binds here are the same at OBS
+active=$(hyprctl clients | grep "com.obsproject.Studio")
 record_bind=Home
-
-hyprctl dispatch focuswindow "class:com.obsproject.Studio"
-wtype -k $record_bind
-hyprctl dispatch workspace previus
-
+if [ -z "$active"  ]; then
+  notify-send "There is no obs session active"
+else 
+  hyprctl dispatch focuswindow "class:com.obsproject.Studio"
+  wtype -k $record_bind
+  hyprctl dispatch workspace previus
+fi
