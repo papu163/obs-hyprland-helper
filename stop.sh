00a1622104d6ebd0d4b445f8bc33450149270927
papu163 <182830006+papu163@users.noreply.github.com>
2025-07-15 16:14:40 -0600

    Update stop.sh

--- stop.sh
+++ stop.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
 
-stop_bind=End
-
-hyprctl dispatch focuswindow "class:com.obsproject.Studio"
-wtype -k $stop_bind
-hyprctl dispatch workspace previus
+#You can use others but first test and check with wtype -k your_bind in your terminal
+#And also you need to check that the binds here are the same at OBS
+active=$(hyprctl clients | grep "com.obsproject.Studio")
+stop_bind=Home
+if [ -z "$active"  ]; then
+  notify-send "There is no obs session active"
+else 
+  hyprctl dispatch focuswindow "class:com.obsproject.Studio"
+  wtype -k $stop_bind
+  hyprctl dispatch workspace previus
+fi
